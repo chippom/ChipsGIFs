6d0b673f618c989f409c91cf87a49350cb029537
Charles J. Pommert Jr. <Chippom2@gmail.com>
2026-02-03 21:43:40 -0500

    made changes to all function files for cloudflare comparability

--- functions/deliver_gif.js
+++ functions/deliver_gif.js
@@ -1,4 +1,4 @@
-import { createClient } from '@supabase/supabase-js';
+import { createClient } from 'https://esm.sh/@supabase/supabase-js@2?bundle';
 
 export async function onRequest(context) {
   const { request, env } = context;
