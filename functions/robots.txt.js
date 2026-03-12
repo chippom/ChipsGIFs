9c3a7d832427d096aa22c78de5112cc5f0ae38c2
Charles J. Pommert Jr. <Chippom2@gmail.com>
2026-03-11 23:47:58 -0400

    Testing removal of the robots.txt.js file Cloudflare keeps choking on

--- functions/robots.txt.js
+++
@@ -1,6 +0,0 @@
-export async function onRequest() {
-  return new Response(
-    "User-agent: *\nAllow: /\nSitemap: https://chips-gifs.com/sitemap.xml\n",
-    { headers: { "Content-Type": "text/plain" } }
-  );
-}
