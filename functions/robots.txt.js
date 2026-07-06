dbd60581f69ce43c813bcb3ef68da85d3f61164d
Charles J. Pommert Jr. <Chippom2@gmail.com>
2026-07-06 15:40:22 -0400

    updated Robots TXT

--- functions/robots.txt.js
+++ functions/robots.txt.js
@@ -1,6 +1,6 @@
 export async function onRequest() {
   return new Response(
-    "User-agent: *\nAllow: /\nSitemap: https://chips-gifs.com/sitemap.xml\n",
+    "User-agent: *\nAllow: /\nSitemap: https://chips-gifs.com/sitemap.xml\n\nUser-agent: Yandex\nDisallow: /*.gif$\n",
     { headers: { "Content-Type": "text/plain" } }
   );
 }
